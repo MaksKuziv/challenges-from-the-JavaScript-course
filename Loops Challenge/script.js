b5e0fa8ea9fe8abafb1b0a6ed7d29597fe6682fa
MaksKuziv <gamer52881@gmail.com>
2023-06-26 18:59:35 +0300

    Loops Challenge

---
+++ Loops Challenge/script.js
@@ -0,0 +1,9 @@
+// Написати ф-цію, яка приймає масив чисел і підносить до квадрату кожне з них
+
+function squareArr(arr){
+    for(numbers of arr){
+        console.log(numbers**2);
+    }
+}
+
+squareArr([1, 2, 3, 4, 5, 6, 7, 8, 9])
